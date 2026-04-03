2d375f11f2cfe90ac634e965b94d04f514d08da7
Monish <monishdas2003@gmail.com>
2026-04-03 18:28:52 -0400

    Sonarqube Setup
    Set up Sonarqube for code smells and bug discoveries which will run from the CI as well.

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,3 +1,7 @@
 export default {
   testEnvironment: "node",
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.js"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"],
 };
